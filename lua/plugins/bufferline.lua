d79ffe337976287b635a5ea04b8a1a39c8fc90b1
czc <zckxccc@gmail.com>
2022-09-08 13:57:13 +0800

    change bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,19 @@
 vim.opt.termguicolors = true
+vim.cmd [[
+nnoremap <silent> gb :BufferLinePick<CR>
+]]
 require("bufferline").setup {
   options = {
-    numbers = "buffer_id"
+    diagnostics = "coc",
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local s = " "
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+            or (e == "warning" and " " or "")
+        s = s .. n .. sym
+      end
+      return s
+    end
   }
 }
+
