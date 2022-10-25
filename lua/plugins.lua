bd9f13fb86d021e95ac515014a7b5edaa44e5903
czc <zckxccc@gmail.com>
2022-10-25 09:38:12 +0800

    formate file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,19 +3,11 @@ return require("packer").startup(function(use)
 
 	use("folke/tokyonight.nvim")
 
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-			"kyazdani42/nvim-web-devicons",
-		},
-	})
+	use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
 
 	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "kyazdani42/nvim-web-devicons" })
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
 
 	use("mfussenegger/nvim-dap")
 
@@ -26,22 +18,13 @@ return require("packer").startup(function(use)
 		end,
 	})
 
-	use({
-		"windwp/nvim-autopairs",
-	})
+	use({ "windwp/nvim-autopairs" })
 
-	use({
-		"lewis6991/gitsigns.nvim",
-		tag = "release",
-	})
+	use({ "lewis6991/gitsigns.nvim", tag = "release" })
 
 	use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+	use({ "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = "nvim-lua/plenary.nvim" })
 
 	use("lukas-reineke/indent-blankline.nvim")
 
@@ -60,21 +43,15 @@ return require("packer").startup(function(use)
 
 	use({ "mhartington/formatter.nvim" })
 
-	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-	})
+	use({ "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
-	use({
-		"glepnir/lspsaga.nvim",
-		branch = "main",
-	})
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
 
-	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
+	use({ "rcarriga/nvim-dap-ui", requires = "mfussenegger/nvim-dap" })
 
 	use("theHamsta/nvim-dap-virtual-text")
 
-	use({ "mxsdev/nvim-dap-vscode-js", requires = { "mfussenegger/nvim-dap" } })
+	use({ "mxsdev/nvim-dap-vscode-js", requires = "mfussenegger/nvim-dap" })
 	use({
 		"microsoft/vscode-js-debug",
 		opt = true,
