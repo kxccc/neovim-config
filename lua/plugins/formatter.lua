6597422d77310c7084511b287d4d6d5f07a51979
czc <zckxccc@gmail.com>
2022-10-28 21:46:25 +0800

    add c/c++ formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -9,33 +9,19 @@ require("formatter").setup({
 	logging = true,
 	log_level = vim.log.levels.WARN,
 	filetype = {
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
+		lua = { require("formatter.filetypes.lua").stylua },
 
-		yaml = {
-			require("formatter.filetypes.yaml").prettier,
-		},
+		yaml = { require("formatter.filetypes.yaml").prettier },
 
-		javascript = {
-			require("formatter.filetypes.javascript").prettier,
-		},
+		javascript = { require("formatter.filetypes.javascript").prettier },
 
-		typescript = {
-			require("formatter.filetypes.typescript").prettier,
-		},
+		typescript = { require("formatter.filetypes.typescript").prettier },
 
-		css = {
-			require("formatter.filetypes.css").prettier,
-		},
+		css = { require("formatter.filetypes.css").prettier },
 
-		json = {
-			require("formatter.filetypes.json").prettier,
-		},
+		json = { require("formatter.filetypes.json").prettier },
 
-		html = {
-			require("formatter.filetypes.html").prettier,
-		},
+		html = { require("formatter.filetypes.html").prettier },
 
 		java = {
 			function()
@@ -43,8 +29,10 @@ require("formatter").setup({
 			end,
 		},
 
-		["*"] = {
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
+		cpp = { require("formatter.filetypes.cpp").clangformat },
+
+		c = { require("formatter.filetypes.c").clangformat },
+
+		["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
 	},
 })
