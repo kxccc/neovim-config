f69b51b56bea9ea5d51f7136a4c66348b476a571
czc <zckxccc@gmail.com>
2023-03-08 11:18:24 +0800

    update packer config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -182,7 +182,7 @@ return require("packer").startup(function(use)
 	use({
 		"microsoft/vscode-js-debug",
 		opt = true,
-		run = "npm install --legacy-peer-deps && npm run compile",
+		run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
 	})
 
 	-- java 工具
