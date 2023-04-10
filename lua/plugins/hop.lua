d6e193f9ff53ab97062503a6e46a68b6cc7bcb1e
czc <zckxccc@gmail.com>
2023-04-10 18:41:27 +0800

    Modify key mappings.

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -24,14 +24,14 @@ return {
 			vim.keymap.set({ "n", "v" }, "<leader>s", function()
 				hop_zh_by_flypy.hint_char2({ direction = directions.AFTER_CURSOR })
 			end, { remap = true })
-			vim.keymap.set({ "n", "v" }, "<leader>S", function()
+			vim.keymap.set({ "n", "v" }, "<leader>a", function()
 				hop_zh_by_flypy.hint_char2({ direction = directions.BEFORE_CURSOR })
 			end, { remap = true })
 
 			vim.keymap.set("o", "<leader>s", function()
 				hop_zh_by_flypy.hint_char2({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
 			end, { remap = true })
-			vim.keymap.set("o", "<leader>S", function()
+			vim.keymap.set("o", "<leader>a", function()
 				hop_zh_by_flypy.hint_char2({ direction = directions.BEFORE_CURSOR, hint_offset = 1 })
 			end, { remap = true })
 
