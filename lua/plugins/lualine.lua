544bf5150bac904773cb03ca833b3bb0a4037edf
czc <zckxccc@gmail.com>
2023-04-12 22:08:27 +0800

    add plugin noice

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,31 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = "kyazdani42/nvim-web-devicons",
+	dependencies = {
+		"kyazdani42/nvim-web-devicons",
+		"folke/noice.nvim",
+	},
 	config = function()
 		require("lualine").setup({
 			options = {
 				theme = "tokyonight",
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.status.message.get_hl,
+						cond = require("noice").api.status.message.has,
+					},
+					{
+						require("noice").api.status.mode.get,
+						cond = require("noice").api.status.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						require("noice").api.status.command.get,
+						cond = require("noice").api.status.command.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
 			},
 		})
 	end,
