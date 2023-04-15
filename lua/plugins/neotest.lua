9321ccdf880fa51787b789988027c7d3b127afaa
czc <zckxccc@gmail.com>
2023-04-15 20:10:06 +0800

    add plugin neotest

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,20 @@
+return {
+	"nvim-neotest/neotest",
+	event = { "VeryLazy" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"antoinemadec/FixCursorHold.nvim",
+		-- adapters
+		"nvim-neotest/neotest-python",
+	},
+	-- FIX: 段错误
+	-- https://github.com/nvim-neotest/neotest/issues/158
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-python")({}),
+			},
+		})
+	end,
+}
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,11 +3,30 @@ return {
 	event = "VeryLazy",
 	config = function()
 		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-		require("which-key").setup({
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+		vim.o.timeoutlen = 500
+		require("which-key").setup({})
+		local wk = require("which-key")
+		wk.register({
+			["<leader>m"] = {
+				name = "Neotest",
+				s = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Neotest summary" },
+				a = "attach",
+				M = "clear_marked",
+				T = "clear_target",
+				d = "debug",
+				D = "debug_marked",
+				e = "expand_all",
+				i = "jumpto",
+				m = "mark",
+				J = "next_failed",
+				o = "output",
+				K = "prev_failed",
+				r = "run",
+				R = "run_marked",
+				O = "short",
+				u = "stop",
+				t = "target",
+			},
 		})
 	end,
 }
