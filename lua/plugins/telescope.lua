6d970ef06c30257711d0fa40bd7d7a0af7175fe3
czc <zckxccc@gmail.com>
2023-11-06 20:40:52 +0800

    Remove fixed version for the telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	version = "0.1.1",
 	dependencies = "nvim-lua/plenary.nvim",
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
